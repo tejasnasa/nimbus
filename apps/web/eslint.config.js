019ca5ec3ff4016b8720928da9e73acb88137e31
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2026-03-21 14:44:29 +0530

    feat: Update package references from @repo to @nimbus across the project

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,4 +1,4 @@
-import { nextJsConfig } from "@repo/eslint-config/next-js";
+import { nextJsConfig } from "@nimbus/eslint-config/next-js";
 
 /** @type {import("eslint").Linter.Config[]} */
 export default nextJsConfig;

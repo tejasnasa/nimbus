377f7079266aa3c87c6f0bb6c4e96c4b95b1e8c6
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2026-03-21 18:54:54 +0530

    Revert "feat: initial commit"
    This reverts commit 3d7880056c84526ac217424ce4c76fdbb2b7adf9.

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,9 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { JetBrains_Mono } from "next/font/google";
-import { cn } from "@nimbus/ui/lib/utils";
-
-const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-mono'});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn("font-mono", jetbrainsMono.variable)}>
+    <html lang="en">
       <body>
         {children}
       </body>

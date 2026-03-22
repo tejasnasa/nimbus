97b75913ef8da72ee038d37fae0b6b024f8ea5a8
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2026-03-22 19:18:59 +0530

    feat: add "Create New Workspace" section with button to Home page

--- apps/web/app/home/page.tsx
+++ apps/web/app/home/page.tsx
@@ -1,3 +1,4 @@
+import Button from "@nimbus/ui/Button";
 import Navbar from "@nimbus/ui/Navbar";
 import ToggleGroup from "@nimbus/ui/ToggleGroup";
 import WorkspaceCard from "@nimbus/ui/WorkspaceCard";
@@ -85,7 +86,6 @@ const workspaces = [
     lastUpdated: "2026-03-19T12:34:56Z",
     members: getRandomMembers(3),
   },
-  
 ];
 
 export default function Home() {
@@ -96,6 +96,19 @@ export default function Home() {
       <ToggleGroup options={["All Workspaces", "My Workspaces"]} />
 
       <section className="mx-28 flex flex-wrap gap-8">
+        <div className="w-[48.5%] p-6 bg-(--card) rounded-lg flex flex-col justify-between text-center py-10">
+          <div>
+            <h2 className="text-3xl font-semibold m-4">Create New Workspace</h2>
+            <p className="text-sm text-(--muted-foreground) text-center">
+              Organise your docs, team, and projects in one place
+            </p>
+          </div>
+
+          <Button size="lg" className="m-2">
+            + Create Workspace
+          </Button>
+        </div>
+
         {workspaces.map((ws) => (
           <WorkspaceCard
             key={ws.slugId}

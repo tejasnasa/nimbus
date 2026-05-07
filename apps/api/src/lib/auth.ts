abb0ca2c93557c92e8010562f0b00b0ea801842e
tejasnasa <tejasnasa2005@gmail.com>
2026-05-07 15:56:57 +0530

    fix: remove betterauth monitoring services

--- apps/api/src/lib/auth.ts
+++ apps/api/src/lib/auth.ts
@@ -1,7 +1,6 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "@nimbus/db";
-import { dash } from "@better-auth/infra";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -29,8 +28,5 @@ export const auth = betterAuth({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
-  },
-  plugins: [
-    dash()
-  ]
+  }
 });
--- apps/web/lib/auth-client.ts
+++ apps/web/lib/auth-client.ts
@@ -1,12 +1,8 @@
 import { plugins } from "@milkdown/kit/preset/commonmark";
 import { createAuthClient } from "better-auth/react";
-import { sentinelClient } from "@better-auth/infra/client";
 
 export const authClient: ReturnType<typeof createAuthClient> = createAuthClient(
   {
-    baseURL: `${process.env.NEXT_PUBLIC_BACKEND_URL}`,
-    plugins: [
-      sentinelClient()
-    ]
+    baseURL: `${process.env.NEXT_PUBLIC_BACKEND_URL}`
   },
 );

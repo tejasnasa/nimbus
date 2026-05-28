59d0b0effdfde0d610b7af3c5692dd02bed55185
tejasnasa <tejasnasa2005@gmail.com>
2026-05-28 07:41:46 +0530

    feat: Create new icons for voice chat

---
+++ packages/ui/src/components/icons/Headphones.tsx
@@ -0,0 +1,12 @@
+export default function Headphones({ className }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className={className}
+    >
+      <path d="M12 2C6.48 2 2 6.48 2 12V20C2 21.1 2.9 22 4 22H7C8.1 22 9 21.1 9 20V15C9 13.9 8.1 13 7 13H4V12C4 7.58 7.58 4 12 4C16.42 4 20 7.58 20 12V13H17C15.9 13 15 13.9 15 15V20C15 21.1 15.9 22 17 22H20C21.1 22 22 21.1 22 20V12C22 6.48 17.52 2 12 2Z" />
+    </svg>
+  );
+}
---
+++ packages/ui/src/components/icons/Microphone.tsx
@@ -0,0 +1,12 @@
+export default function Microphone({ className }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className={className}
+    >
+      <path d="M12 2C9.79 2 8 3.79 8 6V12C8 14.21 9.79 16 12 16C14.21 16 16 14.21 16 12V6C16 3.79 14.21 2 12 2ZM19 12H17C17 14.76 14.76 17 12 17C9.24 17 7 14.76 7 12H5C5 15.53 7.64 18.43 11 18.9V22H13V18.9C16.36 18.43 19 15.53 19 12Z" />
+    </svg>
+  );
+}
